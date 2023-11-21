d09a2c48fb9a8fceca767b4e9877840cc2c858e8
Obarads <id115561@gmail.com>
2023-11-22 00:32:34 +0900

    update

---
+++ scripts/One-2-3-45/scripts_in_container/demo.sh
@@ -0,0 +1,5 @@
+INPUT_PATH=$1
+OUTPUT_PATH=$2
+
+/workspace
+python scripts_in_container/image_to_novelview.py
---
+++ scripts/One-2-3-45/scripts_in_container/setup_package.sh
@@ -0,0 +1,17 @@
+# load conda shell
+cd /workspace
+. ~/miniconda3/etc/profile.d/conda.sh
+
+# setup python and packages
+cd /workspace
+conda create -y -n one-2-3-45 python=3.9
+conda activate one-2-3-45
+echo "conda activate one-2-3-45" >> ~/.bashrc
+
+# download packages (requirements.txt)
+cd /workspace/scripts_in_container
+pip install -r requirements.txt
+
+# download pre-trained model
+cd /workspace
+python download_ckpt.py
